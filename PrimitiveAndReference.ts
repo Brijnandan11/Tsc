ce96999e771e0fbc9a7dc88affeaffcbf7f420d2
Brijnandan11 <brij19069l@gmail.com>
2026-07-08 23:42:32 +0530

    primitve and reference

---
+++ PrimitiveAndReference.ts
@@ -0,0 +1,21 @@
+// Primitive and reference
+
+// Primitve
+var a = 12; //primitive
+
+var b = "Nitesh"; // primitive
+
+var c = 23.43; //primitive
+
+var d = true; // primitve
+
+var e = 'a'; //primitve
+
+
+// Reference
+
+var f = []; // reference
+
+var g = {}; // reference
+
+var t = () => {}; // reference
